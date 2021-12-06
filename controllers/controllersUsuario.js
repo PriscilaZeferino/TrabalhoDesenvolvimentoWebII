c722c456661c690d78eb702216e19adb54662e0e
Priscila Zeferino <45722071+PriscilaZeferino@users.noreply.github.com>
2021-12-06 09:57:57 -0300

    Added Controller Login Authentication

--- controllers/controllersUsuario.js
+++ controllers/controllersUsuario.js
@@ -1,95 +1,102 @@
-const isLoggedIn = true;
+const passport = require("passport");
+
+
+const User = require('../models/user');
+const Meta = require('../models/meta');
+
 
 module.exports = {
 
     async listarMetas(req, res) {
-        if(!isLoggedIn) {
-            res.redirect('login')
-        } else {
-            res.render('meta', {tituloPagina: "Lista de Metas"});
-        }
+        const meta = await Meta.find({});
+        res.render('meta', {tituloPagina: "Lista de Metas", meta})
     },
 
     async mostrarPaginaDeLogin(req, res) {
-        res.render('login', {tituloPagina: "Login"})
-    },
-
-    async enviarDadosDeLogin(req, res) {
-        if(!isLoggedIn) {
-            res.redirect('/');
-        } else {
-            res.redirect('login')
-        }    
+        res.render('login', {tituloPagina: "Login", msg: ''})
     },
 
     async mostrarPaginaDeCadastro(req, res) {
-        if(isLoggedIn) {
-            res.redirect('/');
-        } else {
-            res.render('register', {tituloPagina: "Registrar"})
-        }
+        res.render('register', {tituloPagina: "Registrar", msg: ''})
     },
 
-    async salvarDadosDeCadastro(req, res) {
-        if(isLoggedIn) {
-            res.redirect('/');
-        } else {
-            res.render('register', {tituloPagina: "Registrar"})
-        }
+    async salvarDadosDeCadastro(req, res) {  
+
+            const {username, email, password} = req.body;
+            let msg = '';
+
+            if(username && email && password) {
+                Users = await new User({email: email, username: username});
+                await User.register(Users, password, function(err, user) {
+                        if (err) {
+                            msg = 'Este usuário ja está em uso. Tente fazer login!'
+                            res.render('register', {tituloPagina: "Registrar", msg})
+                        }else{
+                            passport.authenticate("local")(req, res, ()=>{
+                                res.redirect("/");
+                            });
+                        }
+                });
+            } else {
+                msg = 'Por favor, preencha todos os campos!'
+                res.render('register', {tituloPagina: "Registrar", msg})
+            }
+
+
     },
 
     async mostrarPaginaDeCadastroDeMetas(req, res) {
-        if(!isLoggedIn) {
-            res.redirect('/');
-        } else {
-            res.render('cadastrarMeta', {tituloPagina: "Cadastrar Nova Meta"})
-        }
+        res.render('cadastrarMeta', {tituloPagina: "Cadastrar Nova Meta"})
     },
 
     async salvarDadosDaMeta(req, res) {
-        if(!isLoggedIn) {
-            res.redirect('/');
+
+        const body = await {
+            user: req.session.passport.user,
+            title: req.body.titulo,
+            description: req.body.description,
+            total_books: req.body.total_books,
+            deadline: req.body.prazo,
+        };
+
+        if(body) {
+            try {
+                const novaMeta = new Meta(body);
+                await novaMeta.save();
+                res.redirect('/');
+
+            } catch (err) {
+                console.log(err);
+                res.render('cadastrarMeta', {tituloPagina: "Cadastrar Nova Meta", msg: ''})
+            }
         }
+
     },
 
 
     async mostrarPaginaDeEdicaoDeMetas(req, res) {
-        if(!isLoggedIn) {
-            res.redirect('/');
-        } else {
-            res.render('editarMeta', {tituloPagina: "Editar Meta"})
-        }
+        res.render('editarMeta', {tituloPagina: "Editar Meta"})
     },
 
     async atualizarDadosDaMeta(req, res) {
-        if(!isLoggedIn) {
-            res.redirect('/');
-        } else {
-            res.render('editarMeta', {tituloPagina: "Editar Meta"})
-        }
+        res.redirect('/');
+
     },
 
     async atualizarMeta (req, res) {
-        if(!isLoggedIn) {
-            res.redirect('/');
-        } else {
-            res.render('atualizarMeta', {tituloPagina: "Atualizar Meta"})
-        }
+        res.render('atualizarMeta', {tituloPagina: "Atualizar Leitura"})
     },
 
     async deletarMeta(req, res) {
-        if(!isLoggedIn) {
-            res.redirect('/');
-        } else {
-            res.redirect('/listar')
-        }
+        res.redirect('/');
     },
 
     async mostrarPaginaDetalhesMeta(req, res) {
-        if(!isLoggedIn) {
-            res.redirect('/');
-        } else {
-            res.render('detalhesMeta', {tituloPagina: "Detalhes da Meta"})
-        }
+        res.render('detalhesMeta', {tituloPagina: "Detalhes da Meta"})
+    },
+
+    async logout(req, res) {
+        req.logout();
+        res.redirect("/");
     },
 }
