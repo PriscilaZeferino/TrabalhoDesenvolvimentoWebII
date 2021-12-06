63b78c4bba3e3c5e4530f696354448771566d02b
Priscila Zeferino <45722071+PriscilaZeferino@users.noreply.github.com>
2021-12-06 09:56:55 -0300

    Added Login Authentication

--- routes/routesUsuario.js
+++ routes/routesUsuario.js
@@ -1,27 +1,37 @@
 const controllersUsuario = require('../controllers/controllersUsuario')
-
 const express = require('express')
 const router = express.Router()
+const passport = require("passport");
 
-router.get('/', controllersUsuario.listarMetas)
+const isLoggedIn = (req, res, next) => {
+    if(req.isAuthenticated()){
+        return next();
+    } else {
+        res.redirect("/login");
+    }
+}
+
+router.get('/', isLoggedIn, controllersUsuario.listarMetas)
 
 router.get('/login', controllersUsuario.mostrarPaginaDeLogin)
-router.post('/login', controllersUsuario.enviarDadosDeLogin)
+router.post('/login', passport.authenticate("local", {successRedirect: `/`, failureRedirect: "/login"}));
 
 router.get('/register', controllersUsuario.mostrarPaginaDeCadastro)
 router.post('/register', controllersUsuario.salvarDadosDeCadastro)
 
-router.get('/meta/cadastrar', controllersUsuario.mostrarPaginaDeCadastroDeMetas)
-router.post('/meta/cadastrar', controllersUsuario.salvarDadosDaMeta)
+router.get('/meta/cadastrar', isLoggedIn, controllersUsuario.mostrarPaginaDeCadastroDeMetas)
+router.post('/meta/cadastrar', isLoggedIn, controllersUsuario.salvarDadosDaMeta)
 
-router.get('/meta/editar', controllersUsuario.mostrarPaginaDeEdicaoDeMetas)
-router.put('/meta/editar', controllersUsuario.atualizarDadosDaMeta)
+router.get('/meta/:id/editar', isLoggedIn, controllersUsuario.mostrarPaginaDeEdicaoDeMetas)
+router.put('/meta/:id/editar', isLoggedIn, controllersUsuario.atualizarDadosDaMeta)
 
-router.get('/meta/atualizar', controllersUsuario.atualizarMeta)
+router.get('/meta/atualizar', isLoggedIn, controllersUsuario.atualizarMeta)
 
-router.delete('/meta/deletar', controllersUsuario.deletarMeta)
+router.delete('/meta/deletar', isLoggedIn, controllersUsuario.deletarMeta)
 
-router.get('/meta/detalhes', controllersUsuario.mostrarPaginaDetalhesMeta)
+router.get('/meta/detalhes', isLoggedIn, controllersUsuario.mostrarPaginaDetalhesMeta)
+
+router.get('/logout', isLoggedIn, controllersUsuario.logout)
 
 
 module.exports = router;
