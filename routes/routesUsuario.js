4b8310fa1ec723e3daefda113099ec117c18d4b1
Priscila Zeferino <45722071+PriscilaZeferino@users.noreply.github.com>
2021-12-06 13:16:02 -0300

    Updated Routes

--- routes/routesUsuario.js
+++ routes/routesUsuario.js
@@ -11,25 +11,25 @@ const isLoggedIn = (req, res, next) => {
     }
 }
 
-router.get('/', isLoggedIn, controllersUsuario.listarMetas)
-
 router.get('/login', controllersUsuario.mostrarPaginaDeLogin)
 router.post('/login', passport.authenticate("local", {successRedirect: `/`, failureRedirect: "/login"}));
 
 router.get('/register', controllersUsuario.mostrarPaginaDeCadastro)
 router.post('/register', controllersUsuario.salvarDadosDeCadastro)
 
-router.get('/meta/cadastrar', isLoggedIn, controllersUsuario.mostrarPaginaDeCadastroDeMetas)
-router.post('/meta/cadastrar', isLoggedIn, controllersUsuario.salvarDadosDaMeta)
+router.get('/', isLoggedIn, controllersUsuario.listarMetas)
+router.get('/meta/:id/detalhes', isLoggedIn, controllersUsuario.mostrarPaginaDetalhesMeta)
 
-router.get('/meta/:id/editar', isLoggedIn, controllersUsuario.mostrarPaginaDeEdicaoDeMetas)
-router.put('/meta/:id/editar', isLoggedIn, controllersUsuario.atualizarDadosDaMeta)
+router.get('/meta', isLoggedIn, controllersUsuario.mostrarPaginaDeCadastroDeMetas)
+router.post('/meta', isLoggedIn, controllersUsuario.salvarDadosDaMeta)
 
-router.get('/meta/atualizar', isLoggedIn, controllersUsuario.atualizarMeta)
+router.get('/meta/:id', isLoggedIn, controllersUsuario.mostrarPaginaDeEdicaoDeMetas)
+router.patch('/meta/:id', isLoggedIn, controllersUsuario.atualizarDadosDaMeta)
+router.delete('/meta/:id', isLoggedIn, controllersUsuario.deletarMeta)
 
-router.delete('/meta/deletar', isLoggedIn, controllersUsuario.deletarMeta)
+router.get('/meta/:id/atualizar', isLoggedIn, controllersUsuario.atualizarMetaDeLivros)
+router.post('/meta/:id/atualizar', isLoggedIn, controllersUsuario.atualizarQtdDeLivrosLidos)
 
-router.get('/meta/detalhes', isLoggedIn, controllersUsuario.mostrarPaginaDetalhesMeta)
 
 router.get('/logout', isLoggedIn, controllersUsuario.logout)
 
