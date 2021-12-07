e106ab83b4d4ccec6343709774a018efb2fe946b
Priscila Zeferino <45722071+PriscilaZeferino@users.noreply.github.com>
2021-12-06 21:06:19 -0300

    Message Invalid Username or Password

--- routes/routesVisit.js
+++ routes/routesVisit.js
@@ -5,7 +5,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/login', controllersAuthentication.mostrarPaginaDeLogin)
-router.post('/login', passport.authenticate("local", {successRedirect: `/`, failureRedirect: "/login"}));
+router.post('/login', passport.authenticate("local", {successRedirect: `/`, failureRedirect: "/login", failureFlash: 'Invalid username or password'}));
 
 router.get('/register', controllersAuthentication.mostrarPaginaDeCadastro)
 router.post('/register', controllersAuthentication.salvarDadosDeCadastro)
